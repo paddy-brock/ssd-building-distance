e9c2df59d4283a41a05373796d548690d5a42cfc
Patrick Brock <brock@unhcr.org>
2023-01-08 12:31:23 +0100

    re-formatting

--- ssd_building_layer_enhancement.R
+++ ssd_building_layer_enhancement.R
@@ -1,25 +1,30 @@
-# December 2022 and January 2023
-
-# 1. Enhancing the Google Open Buildings layer for South Sudan
-# For host community and refugee sampling for the Forced Displacement Survey
+# Enhancing the Google Open Buildings layer for South Sudan for host community and refugee sampling for the UNHCR Forced Displacement Survey
 
 setwd("C:/Users/BROCK/OneDrive - UNHCR/Documents/South Sudan Scoping/SSD-Building-Distance")
 
 library(sf)
 library(tidyr)
 library(tmap)
-
-# Read in the UNHCR layers for South Sudan including settlement boundaries
+library(lubridate)
 
 # Read in SSD Google Open Buildings layer
+ssd_gob <- read.csv("building layer export.csv",T)[,1:7]
+
+# Re-formatting
+ssd_gob$OBJECTID <- as.factor(ssd_gob$OBJECTID)
+ssd_gob$date <- ym(ssd_gob$last_detection_date)
+
+# Project the GOB layer
+
+# Read in the UNHCR layers for South Sudan including settlement boundaries
 
 # Camp proximity
 # Calculate distance of each building to nearest camp (and tag by camp)
 # Centroid and nearest edge
  
-# Find centroid of polygon
+# Find centroid of polygon (sf... st_centroid)
 
-# Measure distance to nearest polygon edge  
+# Measure distance to nearest polygon edge  (sf st_nearest or st_distance)
 
 # Calculate distance to all other camp centroids
 # Calculate a proxy indicator of distance to all camps, e.g. proportion of 1st to 2nd/3rd/4th/5th distance difference of the 1st distance - split into 1/2 and 1/2345
@@ -30,10 +35,13 @@ library(tmap)
 # e.g. distance to nearest path/road, e.g. WFP HDX data
 # e.g. distance to service locations, e.g. UNHCR borehole data?
 
+# Final processing: add a flag using any of these features that could reliable be used (based on testing data) to exclude buildings from sample set
 
+####
+####
+####
 
-
-# 2. Quality assuring the enumeration data from the South Sudan National Bureau of Statistics (pending data from NBS)
+# Quality assuring the enumeration data from the South Sudan National Bureau of Statistics (pending data from NBS)
 # For host community sampling for the Forced Displacement Survey
 
 # GRID3 UNFPA layer:
