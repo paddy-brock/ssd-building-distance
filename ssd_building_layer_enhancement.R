3ee7852f43fa87e29ddf9d2721dfe06dd947eac3
Patrick Brock <brock@unhcr.org>
2023-01-10 10:40:21 +0100

    assignment in/out of perimeter distance to nearest for those outside

--- ssd_building_layer_enhancement.R
+++ ssd_building_layer_enhancement.R
@@ -1,22 +1,67 @@
 # Enhancing the Google Open Buildings layer for South Sudan for host community and refugee sampling for the UNHCR Forced Displacement Survey
 
 setwd("C:/Users/BROCK/OneDrive - UNHCR/Documents/South Sudan Scoping/SSD-Building-Distance")
-
-library(sf)
-library(tidyr)
-library(tmap)
 library(lubridate)
+library('sf')
+library('tidyr')
+library(tmap)
+library(rgeos)
 
-# Read in SSD Google Open Buildings layer
+# Read in SSD Google Open Buildings layer, Re-format (inc, removing Torit and Magwi)
 ssd_gob <- read.csv("building layer export.csv",T)[,1:7]
-
-# Re-formatting
 ssd_gob$OBJECTID <- as.factor(ssd_gob$OBJECTID)
 ssd_gob$date <- ym(ssd_gob$last_detection_date)
+ssd_gob2 <- subset(ssd_gob,ssd_gob$latitude>7)
+gob <-na.omit(ssd_gob2)
 
-# Project the GOB layer
+# Project the GOB layer and make spatial object
+sp_gob <- st_as_sf(gob,coords=c("longitude","latitude")) %>% 
+  st_set_crs(4326) %>%  st_transform(20135) 
 
-# Read in the UNHCR layers for South Sudan including settlement boundaries
+# check
+tmap_mode("view")
+ tm_basemap("OpenStreetMap") +
+  tm_shape(sp_gob) + tm_dots(col = "black")
+
+# UNHCR camp boundaries, re-project to match Google Open Buildings
+per <- st_read("ssd perimeters.shp")
+st_crs(per)
+sp_per <- per %>% st_transform(20135) 
+
+# Tag building points and in/out of perimeters (including names when inside)
+in.p <- st_join(sp_gob,sp_per)
+sp_gob$inside.per <- in.p$name
+sp_gob$inside.per <- ifelse((is.na(sp_gob$inside.per)==T), "outside",sp_gob$inside.per)
+
+# check
+table(sp_gob$inside.per)
+ tm_basemap("OpenStreetMap") +
+  tm_shape(sp_gob) + tm_dots(col = "inside.per", palette=rainbow(length(unique(sp_gob$inside.per)),alpha=0.5))
+
+# For those outside, distance to nearest perimeter
+sp_gob_out <- subset(sp_gob, is.na(sp_gob$inside.per)==T)
+d.mat <- st_distance(sp_gob, sp_per)
+data.frame(d.mat[,1],sp_gob$inside.per)
+sp_gob_min_d <- NA
+sp_gob_min_d_sett <- NA
+sett.names <- unique(sp_per$name)
+
+for (i in 1:length(d.mat[,1])){
+  sp_gob$min_d[i] <- min(d.mat[i,])
+  sp_gob$min_d_sett[i] <- sett.names[which(d.mat[i,]==min(d.mat[i,]))]
+}
+
+# check
+tmap_mode("view")
+ tm_basemap("OpenStreetMap") +
+  tm_shape(sp_gob) + tm_dots(col = "min_d_sett", palette=rainbow(length(unique(sp_gob$min_d_sett)),alpha=0.5))
+ 
+
+
+# st_nearest...
+# st_nearest_feature() - test this with point inside polygon to see if get negative distance
+# if this doesn'
+
 
 # Camp proximity
 # Calculate distance of each building to nearest camp (and tag by camp)
