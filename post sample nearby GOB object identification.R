8b7393eed55d6c1c84aa369ca728bb3261f24750
Patrick Brock <brock@unhcr.org>
2023-01-13 17:14:04 +0100

    addition of sampling procedure

--- post sample nearby GOB object identification.R
+++ post sample nearby GOB object identification.R
@@ -14,14 +14,28 @@ library(readr)
 # These should be provided to enumerators such that they can identify whether they are associated with households
 # This information collected by enumerators should then be used to calculate sample weights
 
-# Refugees, example sample of 3000 on OBJECTID
-gob.en <- readr::read_csv("SSD Unity Upper Nile Google Open Buildings enhanced.csv",T)
-ref.en <- subset(gob_en, gob_en$inside_per!="outside")
-refsam <- sample(ref_en$OBJECTID,3000,replace=F)
+# Refugees, example sample of n on OBJECTID
+gob.en. <- readr::read_csv("SSD Unity Upper Nile Google Open Buildings enhanced.csv",T)
+gob.en <- st_as_sf(gob.en.,coords=c("longitude","latitude")) %>% 
+  st_set_crs(4326) %>%  st_transform(20135) 
+ref.en <- subset(gob.en, gob_en$inside_per!="outside")
+n <- 3000
+refsam <- sample(ref.en$OBJECTID,n,replace=F)
+rsm <- match(ref.en$OBJECTID,refsam)
+ref.en$sample<-NA
+ref.en$sample <- ifelse(is.na(rsm)==T,"no","yes")
+table(ref.en$sample)
 
+# For each sampled building object, identify all those that are within threshold.m meters
+threshold.m <- 20
 
+# calculate distances
 
-# For each location in sample, find and label 
+for(i in 1:length(refsam)){
+  tds <- subset(ref.en,ref.en$OBJECTID==refsam[i])
+  tdmat <- st_distance(tds,ref.en)
+}
+
 
 
 # Host communities, example sample of 3000 on OBJECTID
